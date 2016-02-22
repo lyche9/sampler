a77d0394882fe970d1af9482a3d11fe28598a1d0
lyche9 <lychen518@gmail.com>
2016-02-22 14:48:20 -0500

    rudimentary API call working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+var google = require('googleapis');
+var youtube = google.youtube('v3');
+
+var API_KEY = require("../key.json");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/*POST to index*/
+router.post('/', function(req, res) {
+
+  console.log(req.body);
+
+  /*Make API call*/
+  var params = {
+    key:        API_KEY.key,
+    part:       "snippet",
+    q:          req.body.test,
+    type:       "video",
+    maxResults: 5
+  };
+
+  youtube.search.list(params, function(err, response) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      for (var i in response.items) {
+        console.log(response.items[i]);
+      }
+    }
+  });
+
+
+
+});
+
 
 module.exports = router;
